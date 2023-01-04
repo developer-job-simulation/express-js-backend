a4b2a5fe0bc3de0295bed0f5e83e2f6d73894ec0
JukemJ <chris@dixn.dev>
2023-01-04 02:37:13 -0500

    fixed routes

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -7,6 +7,35 @@ router.get("/", function (req, res, next) {
   res.json(pokedex);
 });
 
+/* GET Pokemon by HP */
+router.get("/hp", function (req, res, next) {
+  let pokemon = pokedex
+  for(let q in req.query){
+    switch (q){
+      case 'gte':{
+        pokemon = pokemon.filter(x => x.base.HP >= req.query[q])
+        break
+      }
+      case 'lte':{
+        pokemon = pokemon.filter(x => x.base.HP <= req.query[q])
+        break
+      }
+      case 'gt':{
+        pokemon = pokemon.filter(x => x.base.HP > req.query[q])
+        break
+      }
+      case 'lt':{
+        pokemon = pokemon.filter(x => x.base.HP < req.query[q])
+        break
+      }
+      default: res.json({"error": "Invalid Operator. Must be one of [\"gt\",\"gte\",\"lt\",\"lte\"]"})
+    }
+  }
+  if(!pokemon) res.json({"error": "Not found"})
+  else res.json(pokemon)
+  return;
+});
+
 /* GET Pokemon by Id. */
 router.get("/:id", function (req, res, next) {
   let pokemon = pokedex.find(x => x.id == req.params.id)
@@ -30,14 +59,6 @@ router.get("/type/:type", function (req, res, next) {
   let pokemon = pokedex.filter(x => x.type == type)
   if(!pokemon) res.json({"error": "Not found"})
   else res.json(pokemon)
-  return;
-});
-
-/* GET Pokemon by HP */
-router.get("/:hp", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  console.log(req.query)
-  res.json({ message: "Not Implemented" });
   return;
 });
 

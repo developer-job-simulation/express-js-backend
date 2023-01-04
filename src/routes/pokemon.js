7f638d9cf1311aacf047b6c9a8916b9c7ce40709
JukemJ <chris@dixn.dev>
2023-01-03 19:18:15 -0500

    fixed 3 of 4 routes

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -9,29 +9,35 @@ router.get("/", function (req, res, next) {
 
 /* GET Pokemon by Id. */
 router.get("/:id", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  let pokemon = pokedex.find(x => x.id == req.params.id)
+  if(!pokemon) res.json({"error": "Not found"})
+  else res.json(pokemon)
   return;
 });
 
 /* GET Pokemon by English Name */
 router.get("/name/:name", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  let name = req.params.name[0].toUpperCase() + req.params.name.slice(1).toLowerCase()
+  let pokemon = pokedex.find(x => x.name.english == name)
+  if(!pokemon) res.json({"error": "Not found"})
+  else res.json(pokemon)
   return;
 });
 
 /* GET Pokemon by Type */
 router.get("/type/:type", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  let type = req.params.type[0].toUpperCase() + req.params.type.slice(1).toLowerCase()
+  let pokemon = pokedex.filter(x => x.type == type)
+  if(!pokemon) res.json({"error": "Not found"})
+  else res.json(pokemon)
   return;
 });
 
 /* GET Pokemon by HP */
-router.get("/hp", function (req, res, next) {
+router.get("/:hp", function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  console.log(req.query)
+  res.json({ message: "Not Implemented" });
   return;
 });
 

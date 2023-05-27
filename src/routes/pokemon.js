48ac0e9503b3b3f482961903bab1cae3db7e6723
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-05-26 17:37:55 -0700

    fixed bugs with implementation of Get Pokemon by ID

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -11,14 +11,17 @@ router.get('/', function (req, res, next) {
 router.get('/:id', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
 
-  const id = Number(req.params.id);
-  // console.log('id: ', typeof id, id);
-  if (typeof id !== 'number') {
+  const id = req.params.id;
+
+  if (isNaN(id)) {
     res.status(400).json({ error: 'Invalid ID' });
+    return next();
   }
   if (!pokedex[id]) {
-    res.status(400).json({ error: 'Not found' });
+    res.status(404).json({ error: 'Not found' });
+    return next();
   }
+
   res.status(200).json(pokedex[id - 1]);
   return next();
 });

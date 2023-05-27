0e37d90acc12ca98d95a361c292f2746a9234e55
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2023-05-26 22:59:04 -0700

    implemented Get Pokemon by HP

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -7,25 +7,6 @@ router.get('/', function (req, res, next) {
   res.json(pokedex);
 });
 
-/* GET Pokemon by Id. */
-router.get('/:id', function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-
-  const id = req.params.id;
-
-  if (isNaN(id)) {
-    res.status(400).json({ error: 'Invalid ID' });
-    return next();
-  }
-  if (!pokedex[id]) {
-    res.status(404).json({ error: 'Not found' });
-    return next();
-  }
-
-  res.status(200).json(pokedex[id - 1]);
-  return next();
-});
-
 /* GET Pokemon by English Name */
 router.get('/name/:name', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
@@ -46,8 +27,7 @@ router.get('/name/:name', function (req, res, next) {
     return next();
   }
 
-  res.status(200).json(result);
-  return next();
+  return res.status(200).json(result);
 });
 
 /* GET Pokemon by Type */
@@ -65,15 +45,66 @@ router.get('/type/:type', function (req, res, next) {
     res.status(404).json({ error: 'Bad request' });
     return next();
   }
-  res.status(200).json(result);
-  return next();
+  return res.status(200).json(result);
 });
-
 /* GET Pokemon by HP */
 router.get('/hp', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: 'Not Implemented' });
-  return;
+
+  console.log('query: ', req.query);
+
+  const { gt, lt, lte, gte } = req.query;
+  console.log('gt: ', gt);
+  console.log('lt: ', lt);
+  let result;
+
+  if (gt) {
+    result = pokedex.filter((pokemon) => {
+      return pokemon.base.HP > parseInt(gt);
+    });
+  }
+  if (lt) {
+    result = pokedex.filter((pokemon) => {
+      return pokemon.base.HP < parseInt(lt);
+    });
+  }
+  if (gte) {
+    result = pokedex.filter((pokemon) => {
+      return pokemon.base.HP >= parseInt(gt);
+    });
+  }
+  if (lte) {
+    result = pokedex.filter((pokemon) => {
+      return pokemon.base.HP <= parseInt(gt);
+    });
+  }
+
+  if (!gt && !lt && !gte && !lte) {
+    res.status(400).json({
+      error: 'Invalid Operator. Must be one of ["gt","gte","lt","lte"]',
+    });
+    return;
+  }
+  console.log('result: ', result);
+
+  return res.status(200).json(result);
+});
+/* GET Pokemon by Id. */
+router.get('/:id', function (req, res, next) {
+  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
+
+  const id = req.params.id;
+
+  if (isNaN(id)) {
+    res.status(400).json({ error: 'Invalid ID' });
+    return next();
+  }
+  if (!pokedex[id]) {
+    res.status(404).json({ error: 'Not found' });
+    return next();
+  }
+
+  return res.status(200).json(pokedex[id - 1]);
 });
 
 module.exports = router;

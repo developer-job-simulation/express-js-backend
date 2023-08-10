4005ed32c4ed35494615eb4e861ac2494d1a3760
Anton Gundermann <anton.gundermann@gmx.de>
2023-08-10 12:24:14 +0200

    fixed: issues #1, #2 and #3

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -1,38 +1,60 @@
-const express = require("express");
-const router = express.Router();
-const pokedex = require("../db/pokedex.json");
+const express = require('express')
+const router = express.Router()
+const pokedex = require('../db/pokedex.json')
 
 /* GET All Pokemon */
-router.get("/", function (req, res, next) {
-  res.json(pokedex);
-});
+router.get('/', function (req, res, next) {
+  res.json(pokedex)
+})
 
 /* GET Pokemon by Id. */
-router.get("/:id", function (req, res, next) {
+router.get('/:id', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
-});
+  const id = req.params.id
+  if (!Number(id)) {
+    return res.status(400).json({ error: 'Invalid ID' })
+  }
+
+  const foundPokemon = pokedex.find((item) => item.id === Number(id))
+  if (foundPokemon) {
+    return res.status(200).json(foundPokemon)
+  }
+
+  return res.status(404).json({ message: 'Not found' })
+})
 
 /* GET Pokemon by English Name */
-router.get("/name/:name", function (req, res, next) {
+router.get('/name/:name', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
-});
+  const name = req.params.name
+  const foundPokemon = pokedex.find(
+    (item) => item.name.english.toLowerCase() === name.toLowerCase()
+  )
+  if (foundPokemon) {
+    return res.status(200).json(foundPokemon)
+  }
+  return res.status(404).json({ message: 'Not found' })
+})
 
 /* GET Pokemon by Type */
-router.get("/type/:type", function (req, res, next) {
+router.get('/type/:type', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
-});
+  let type = req.params.type
+  type = type.charAt(0).toUpperCase() + type.slice(1)
+  const foundPokemon = pokedex.filter((item) => item.type.includes(type))
+  if (foundPokemon.length > 0) {
+    return res.status(200).json(foundPokemon)
+  }
+  return res.status(400).json({ message: 'Bad request' })
+})
 
 /* GET Pokemon by HP */
-router.get("/hp", function (req, res, next) {
+router.get('/hp', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
-  return;
-});
+  console.log(req)
 
-module.exports = router;
+  res.status(501).json(req)
+  return
+})
+
+module.exports = router

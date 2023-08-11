35cda079a3dede8eeff4a6782beb0f251139d462
Anton Gundermann <anton.gundermann@gmx.de>
2023-08-11 20:26:47 +0200

    fixed: issues #4

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -7,22 +7,6 @@ router.get('/', function (req, res, next) {
   res.json(pokedex)
 })
 
-/* GET Pokemon by Id. */
-router.get('/:id', function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  const id = req.params.id
-  if (!Number(id)) {
-    return res.status(400).json({ error: 'Invalid ID' })
-  }
-
-  const foundPokemon = pokedex.find((item) => item.id === Number(id))
-  if (foundPokemon) {
-    return res.status(200).json(foundPokemon)
-  }
-
-  return res.status(404).json({ message: 'Not found' })
-})
-
 /* GET Pokemon by English Name */
 router.get('/name/:name', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
@@ -51,10 +35,49 @@ router.get('/type/:type', function (req, res, next) {
 /* GET Pokemon by HP */
 router.get('/hp', function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  console.log(req)
+  const query = req.query
+  const filter = Object.keys(query)[0]
+  const value = Number(query[filter])
+  let foundPokemon = []
+  switch (filter) {
+    case 'gte':
+      foundPokemon = pokedex.filter((item) => item.base.HP >= value)
+      break
+    case 'gt':
+      foundPokemon = pokedex.filter((item) => item.base.HP > value)
+      break
+    case 'lt':
+      foundPokemon = pokedex.filter((item) => item.base.HP < value)
+      break
+    case 'lte':
+      foundPokemon = pokedex.filter((item) => item.base.HP <= value)
+      break
+    default:
+      return res.status(400).json({
+        error: 'Invalid Operator. Must be one of ["gt","gte","lt","lte"]',
+      })
+  }
+  if (foundPokemon.length) {
+    return res.status(200).json(foundPokemon)
+  }
+  return res.status(404).json({ message: 'Not found' })
+})
 
-  res.status(501).json(req)
-  return
+/* GET Pokemon by Id. */
+router.get('/:id', function (req, res, next) {
+  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
+  const id = req.params.id
+  const isNumber = id.match(/\d+/)
+  if (!isNumber) {
+    return res.status(400).json({ error: 'Invalid ID' })
+  }
+
+  const foundPokemon = pokedex.find((item) => item.id === Number(id))
+  if (foundPokemon) {
+    return res.status(200).json(foundPokemon)
+  }
+
+  return res.status(404).json({ message: 'Not found' })
 })
 
 module.exports = router

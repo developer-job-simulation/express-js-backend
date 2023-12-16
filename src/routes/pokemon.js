b3026757e55a849c201f5153d96db9daf819a225
Ajelmar Medina <ajelmarmedina@gmail.com>
2023-12-16 15:39:12 +0800

    HotFix

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pokedex = require("../db/pokedex.json");
+const { search } = require("./pokemon");
 
 /* GET All Pokemon */
 router.get("/", function (req, res, next) {
@@ -9,36 +10,38 @@ router.get("/", function (req, res, next) {
 
 /* GET Pokemon by HP */
 router.get("/hp", function (req, res, next) {
-  const queryKeys = Object.keys(req.query)
-  if (queryKeys.length > 1 || !queryKeys.length) res.status(404).json({ error: "Not found" });
-  let predicate;
-  switch (queryKeys[0]) {
-    case 'gt': predicate = pokemon => pokemon.base.HP  > req.query["gt"];
-      break;
-    case 'lt': predicate = pokemon => pokemon.base.HP  < req.query["lt"];
-      break;
-    case 'gte': predicate = pokemon => pokemon.base.HP  > req.query["gte"];
-      break;
-    case 'lte': predicate = pokemon => pokemon.base.HP  < req.query["lte"];
-      break;
-    default: res.status(400).json({ error: 'Invalid Operator. Must be one of ["gt","gte","lt","lte"]' })
-      break;
+  const queryKeys = Object.keys(req.query);
+  if (!queryKeys.length) return res.status(404).json({ error: "Not found" });
+
+  /**
+   * @returns {Boolean} if a given value tests valid with the respective query
+   */
+  function testQuery(query, value) {
+    switch (query) {
+      case 'gt': return value > req.query["gt"];
+      case 'lt': return value < req.query["lt"];
+      case 'gte': return value > req.query["gte"];
+      case 'lte': return value < req.query["lte"];
+      default: res.status(400).json({ error: 'Invalid Operator. Must be one of ["gt","gte","lt","lte"]' })
+    }
   }
-  const searchResults = pokedex.filter(predicate);
-  res.status(200).json(searchResults)
+
+  const searchResults = pokedex.filter(pokemon =>
+    queryKeys.reduce((isPassed, query) =>
+      isPassed = isPassed ? testQuery(query, pokemon.base.HP) : false
+      , true));
+  if (searchResults.length) res.status(200).json(searchResults);
+  else res.status(404).json({ error: "Not found" })
   return;
 });
 
 /* GET Pokemon by Id. */
 router.get("/:id", function (req, res, next) {
-  try {
-    const id = parseInt(req.params.id);
-    const searchResult = pokedex.find(pokemon => pokemon.id === id);
-    if (searchResult) res.status(200).json(searchResult);
-    else res.status(404).json({ error: "Not found" });
-  } catch (e) {
-    res.status(400).json({ error: "Invalid ID" });
-  }
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) return res.status(400).json({ error: "Invalid ID" });
+  const searchResult = pokedex.find(pokemon => pokemon.id === id);
+  if (searchResult) res.status(200).json(searchResult);
+  else res.status(404).json({ error: "Not found" });
   return;
 });
 

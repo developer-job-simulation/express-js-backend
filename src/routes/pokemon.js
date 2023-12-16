74910b4b65e97125e30848693362aaeb5f8425f7
Ajelmar Medina <ajelmarmedina@gmail.com>
2023-12-16 14:28:10 +0800

    #4 Get Pokemon by HP

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -7,6 +7,28 @@ router.get("/", function (req, res, next) {
   res.json(pokedex);
 });
 
+/* GET Pokemon by HP */
+router.get("/hp", function (req, res, next) {
+  const queryKeys = Object.keys(req.query)
+  if (queryKeys.length > 1 || !queryKeys.length) res.status(404).json({ error: "Not found" });
+  let predicate;
+  switch (queryKeys[0]) {
+    case 'gt': predicate = pokemon => pokemon.base.HP  > req.query["gt"];
+      break;
+    case 'lt': predicate = pokemon => pokemon.base.HP  < req.query["lt"];
+      break;
+    case 'gte': predicate = pokemon => pokemon.base.HP  > req.query["gte"];
+      break;
+    case 'lte': predicate = pokemon => pokemon.base.HP  < req.query["lte"];
+      break;
+    default: res.status(400).json({ error: 'Invalid Operator. Must be one of ["gt","gte","lt","lte"]' })
+      break;
+  }
+  const searchResults = pokedex.filter(predicate);
+  res.status(200).json(searchResults)
+  return;
+});
+
 /* GET Pokemon by Id. */
 router.get("/:id", function (req, res, next) {
   try {
@@ -35,13 +57,6 @@ router.get("/type/:type", function (req, res, next) {
   const searchResults = pokedex.filter(pokemon => pokemon.type.find(type => type.toLowerCase() === qType));
   if (searchResults.length) res.status(200).json(searchResults);
   else res.status(400).json({ error: "Bad request" });
-  return;
-});
-
-/* GET Pokemon by HP */
-router.get("/hp", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
   return;
 });
 

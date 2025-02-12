ba871a585e93c6bb8538231f577fd04a2b015127
Lucky Khoza <84252978+LuckyKhoza-crypto@users.noreply.github.com>
2025-02-12 13:38:14 -0800

    fixed all issues, except last one

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -10,28 +10,55 @@ router.get("/", function (req, res, next) {
 /* GET Pokemon by Id. */
 router.get("/:id", function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  const pkid = parseInt(req.params.id);
+  const pokemon = pokedex.find(({ id }) => id === pkid);
+
+  if (!pokemon) {
+    res.status(501).json({ message: "Not Implemented" });
+  }
+  res.json(pokemon);
   return;
 });
 
 /* GET Pokemon by English Name */
 router.get("/name/:name", function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  const pkname = req.params.name;
+  const pokemon = pokedex.find(({ name }) => name.english === pkname);
+
+  if (!pokemon) {
+    res.status(501).json({ message: "Not Implemented" });
+  }
+  console.log(pkname);
+  res.json(pokemon);
   return;
 });
 
 /* GET Pokemon by Type */
 router.get("/type/:type", function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  const pkType = req.params.type;
+
+  const pokemon = pokedex.filter(({ type }) => type.includes(pkType));
+  console.log(pkType);
+
+  if (!pokemon) {
+    res.status(501).json({ message: "Not Implemented" });
+  }
+  res.json(pokemon);
   return;
 });
 
 /* GET Pokemon by HP */
 router.get("/hp", function (req, res, next) {
   // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  const pkHp = parseInt(req.query.hp);
+  const pokemon = pokedex.filter(({ base }) => base.HP <= pkHp);
+
+  if (!pokemon) {
+    res.status(501).json({ message: "Not Implemented" });
+  }
+  res.json(pokemon);
   return;
 });
 
